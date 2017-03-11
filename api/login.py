5979dd458c5f67b3ef950c0945e90d8ba40dc0ae
ava <ava@web-server.com>
2017-03-11 10:37:19 +0200

    Login finished | added json response

--- api/login.py
+++ api/login.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from flask import Blueprint,request
+from flask import Blueprint,Response,request
 import MySQLdb
+import json
 
 appLogin = Blueprint('api_login',__name__)
 
@@ -10,22 +11,32 @@ def login():
 	db= MySQLdb.connect(host="localhost",user="root", passwd="QAZxsw1234", db="linksdb")
 	
 	status = "error"
+	response ={}
+
+	#get the info from request
 	email = request.form.get("email")
 	password = request.form.get("password")
 	
+	#SQL cmd
 	query =  "SELECT email, password FROM users WHERE email='%s' AND password ='%s'" % (email, password)
 	
+	#execute SQL cmd
 	cursor = db.cursor()
 	cursor.execute(query)
 	
+	#retrieve DB answer
 	data = cursor.fetchone()
 	
 	if data != None:
 		status = "ok"
 	
+	#update json
+	response["status"] = status
+	response["email"] = email
+	response["password"] = password
+
 	# disconnect from server
 	db.close()
-	
-	return status
+	return Response(json.dumps(response, sort_keys=True), mimetype="application/json")
 
 	
