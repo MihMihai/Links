62855ffde93c0bdd231e9e615bf278dc3477f15e
Mihai Borcan <bmihai04@gmail.com>
2017-03-12 13:49:58 +0200

    adding auth token to login - first try

--- api/login.py
+++ api/login.py
@@ -2,6 +2,7 @@
 from flask import Blueprint,Response,request #,redirect,url_for,render_template
 import MySQLdb
 import json
+import jwt
 
 appLogin = Blueprint('api_login',__name__)
 
@@ -36,8 +37,32 @@ def login():
 	db.close()
 
 	#return redirect(urlfor("api_login.chat"))
-	return Response(json.dumps(response, sort_keys=True), mimetype="application/json"),401
+	if "error" in response:
+		return Response(json.dumps(response, sort_keys=True), mimetype="application/json"),401
+	
+	authorizationToken = encode_auth_token(data["id"])
+	
+	return Response(json.dumps(response, sort_keys=True), mimetype="application/json")
 
+def encode_auth_token(self,user_id):
+	"""
+		Generates the Auth Token
+		:return: string
+	"""
+	
+	try:
+		payload = {
+			'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0,seconds=60),
+			'iat': datetime.datetime.utcnow(),
+			'sub': user_id
+		}
+		return jwt.encode(payload,
+			app.config.get("\xf9'\xe4p(\xa9\x12\x1a!\x94\x8d\x1c\x99l\xc7\xb7e\xc7c\x86\x02MJ\xa0"),
+			algorithm = 'HS256'
+		)
+	except Exception as e:
+		return e
+	
 #@appLogin.route("/chat")
 #def chat():
 #	return render_template("index2.html")
