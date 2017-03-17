037aec91aa978df220f6fe91ce70063f2b3b8442
AVA <adevaratulandrei@gmail.com>
2017-03-17 18:53:05 +0200

    added profile request

---
+++ api/profile.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+from flask import Blueprint,Response,request
+import json
+import MySQLdb
+import jwt
+
+
+appProfile = Blueprint('api_profile',__name__)
+
+@appProfile.route("/api/profile",methods=['GET'])
+def friendRequests():
+	response = {}
+
+	db = MySQLdb.connect(host="localhost",user="root",passwd="QAZxsw1234", db="linksdb")
+
+	userToken = request.headers.get("Authorization")
+
+	if userToken == None:
+		response["error"] = "Request does not contain an access token"
+		response["description"] = "Authorization required"
+		response["status_code"] = 401
+		return Response(json.dumps(response,sort_keys=True),mimetype="application/json")
+
+	f = open('server.conf','r')
+	key = f.readline()
+	
+	try:
+		userAcc = jwt.decode(userToken,key)
+	except jwt.ExpiredSignatureError:
+		response["error"] = "Invalid token"
+		response["description"] = "Token has expired"
+		response["status_code"] = 401
+		return Response(json.dumps(response,sort_keys=True),mimetype="application/json"),401
+	except jwt.InvalidTokenError:
+		response["error"] = "Invalid token"
+		response["description"] = "Invalid token"
+		response["status_code"] = 401
+		return Reponse(json.dumps(response,sort_keys=True),mimetype="application/json"),401
+		
+	query = " SELECT name, email, birthday_date FROM users where ID = '%s'" % (userAcc["sub"])
+	
+	cursor = db.cursor()
+	cursor.execute(query)
+	data = cursor.fetchone()
+	
+	response['name']=data[0]
+	response['email']=data[1]
+	response['birthday']=data[2]
+	response['status']='ok'
+	
+	return Response(json.dumps(response,sort_keys=True),mimetype="application/json")
+
+	
