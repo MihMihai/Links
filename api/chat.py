1587d11888754df310eb245a7960c0a54711b5fe
web-server <links@web-server.com>
2017-03-29 00:39:05 +0300

    Added randomchat -- it works ;) + Added online key-value to friends request and functionality for online (if user is online)

--- api/chat.py
+++ api/chat.py
@@ -75,6 +75,11 @@ def on_join(data):
 	cursor.execute(query)
 	token = cursor.fetchone()
 	room = token[0]
+
+	query = "UPDATE users SET online=1 WHERE email = '%s'" % (email)
+	cursor.execute(query)
+	db.commit()
+
 	db.close()
 	join_room(room)
 	emit('msg server',email + ' has entered the room.', room=room)
@@ -89,6 +94,11 @@ def on_leave(data):
 	cursor.execute(query)
 	token = cursor.fetchone()
 	room = token[0]
+
+	query = "UPDATE users SET online=null WHERE email='%s'" % (email)
+	cursor.execute(query)
+	db.commit()
+
 	db.close()
 	leave_room(room)
 	emit('msg server',email + ' has left the room.', room=room)
--- api/randomchat.py
+++ api/randomchat.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-from flask import Blueprint,response,Request
+from flask import Blueprint,Response,request
 import MySQLdb
 import jwt
 import json
 import random
 
-appRandom = Blueprint('api_randomchat',__name__)
+appRandomChat = Blueprint('api_randomchat',__name__)
 
-@appRandom.route('/api/random_chat',methods=['GET'])
+@appRandomChat.route('/api/random_chat',methods=['GET'])
 def randomChat():
 	response = {}
-	
+
 	userToken = request.headers.get("Authorization")
-	
+
 	if userToken == None:
 		response["error"] = "Request does not contain an access token"
 		response["description"] = "Authorization required"
 		response["status_code"] = 401
 		return Response(json.dumps(response,sort_keys=True),mimetype="application/json"), 401
-		
-		
+
 	f = open('server.conf','r')
 	key = f.readline()
-	
+
 	try:
 		userAcc = jwt.decode(userToken,key)
 	except jwt.ExpiredSignatureError:
@@ -35,34 +34,33 @@ def randomChat():
 		response["description"] = "Invalid token"
 		response["status_code"] = 401
 		return Reponse(json.dumps(response,sort_keys=True),mimetype="application/json"),401
-	
+
 	db = MySQLdb.connect(host="localhost",user="root",passwd="QAZxsw1234",db="linksdb")
-	
-	query = "SELECT id FROM users WHERE id NOT IN (SELECT user_1 FROM friendships WHERE user_2 == '%s' UNION SELECT user_2 FROM friendships WHERE user_1 == '%s')" % (userAcc['sub'],userAcc['sub'])
-	
+
+	query = "SELECT id FROM users WHERE id NOT IN (SELECT user_1 FROM friendships WHERE user_2 = '%s' UNION SELECT user_2 FROM friendships WHERE user_1 = '%s')" % (userAcc['sub'],userAcc['sub'])
+
 	cursor = db.cursor()
 	cursor.execute(query)
 	userIdsRow = cursor.fetchall()
-	
-	userIds = [] 
-	
+
+	userIds = []
+
 	for row in userIdsRow:
 		userIds.append(row[0])
-	
+
+	userIds.remove(userAcc['sub'])
+
 	randomId = random.choice(userIds)
-	return randomId
-	
+
 	randomToken = str(encode_chat_token(randomId))
 	randomToken = randomToken[2:]
 	randomToken = randomToken[:len(randomToken)-1]
-	
-	return randomEmail
-	
+
 	response["status"] = 'ok'
 	response["random_token"] = randomToken
-	
+
 	return Response(json.dumps(response,sort_keys=True),mimetype="application/json")
-	
+
 def encode_chat_token(id):
 	#this may throw an exception if file doesn't exist
 	f = open('server.conf','r')
