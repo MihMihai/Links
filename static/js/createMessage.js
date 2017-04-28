6b9bc1a180ddc55a857491af01f94165ab00a8f7
Bogdanel <bogdanalexandrescu96@gmail.com>
2017-04-29 00:27:13 +0300

    fixed bug: long words(length>40)  are split into smaller pieces(length 20 each)

--- static/js/createMessage.js
+++ static/js/createMessage.js
@@ -4,7 +4,7 @@ function Message(msg,sender){
 }
 
 function createMessage(message, sender){
-	let mesaj = $("<p>"+message+"</p>");
+	let mesaj = $("<p>"+resizeMessage(message)+"</p>");
 	let messageDiv = $("<div class='chat-body clearfix'></div>");
 	let li = $("<li class='"+sender+" clearfix'></li>");
 	messageDiv.append(mesaj);
@@ -14,3 +14,19 @@ function createMessage(message, sender){
 	chatBox.scrollTop = chatBox.scrollHeight;
 }
 
+function resizeMessage(message){
+	let messages = message.split(" ");
+	for(let i=0; i<messages.length; i++){
+		if(messages[i].length > 40) {
+			let copyMsg = messages[i];
+			messages.splice(i,1);
+			for(let j=0;j<copyMsg.length;j+=20){
+			//let half2 = messages[i].substr(messages[i].length/2+1, messages[i].length);
+			messages.splice(i+j/20,0,copyMsg.substr(j,20));
+			}
+			i=i+copyMsg.length/20+1;
+		}
+		
+	}
+	return messages.join(" ");
+}
