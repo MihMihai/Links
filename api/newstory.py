95ac0c7b78addb32df218fcc9860b10a5ef44c74
Mihaaai <ghidome@gmail.com>
2017-05-09 23:48:03 +0300

    New story updated -- needs testing

--- api/newstory.py
+++ api/newstory.py
@@ -1,10 +1,9 @@
+#!/usr/bin/python3
+
 # @params : text 
 # return status : "ok"
 
-
-#!usr/bin/python3
-
-from flask import Blueprin, Response, request
+from flask import Blueprint, Response, request
 
 import json
 import jwt
@@ -51,38 +50,47 @@ def newStory() :
 	cursor = db.cursor()
 
 	#get user id
-	query = "SELECT id FROM users WHERE auth_token = %s" % (userToken)
+	query = "SELECT id FROM users WHERE auth_token = '%s'" % (userToken)
 	cursor.execute(query)
 	userId = cursor.fetchone()
 
 	#get text for story
 	storyText = request.form.get('text')
-	if storyText == None or storyText == "" : 
-		response['error'] = "Invalid story text"
-		response['description'] = "Please insert text to your story"
-		response['status_code'] = 400
-		return Response(json.dumps(response,sort_keys = True),mimetype = 'application/json'),400
 
 	#trim text
 	#textMax = 200
 	#storyText = storyText[:textMax]
 
+	#get story image
+	storyImage = request.form.get('image')
+
+	#get story status 
+	storyFeel = request.form.get('status_feel')
+
 	#get current time
 	curdate = time.strftime("%Y-%m-%d")
 
 	#check if user had a story posted
-	query ="SELECT user_id FROM story WHERE user_id = %d" % (userId)
+	query ="SELECT user_id FROM story WHERE user_id = '%d'" % (userId)
 	cursor.execute(query)
 	data = cursor.fetchone()
+
+	add = True;
+	if (storyText == None or storyText == "" ) and \
+	(storyImage == None or storyImage == "") and (storyFeel == None or storyFeel == "") :
+		add = False;
+
 	#if no story there
 	#insert new story in db
-	if data == None :
-		query = "INSERT INTO story (user_id, text, date) VALUES (%d, %s, %s) " % (userId, storyText, curdate)
-		cursor.execute(query)
+	if data[0] == None :
+		if add == True :
+			query = "INSERT INTO story (user_id, text, date) VALUES ('%d', '%s', '%s') " % (userId[0], storyText, curdate)	
+			cursor.execute(query)
 	#else update old story
 	else :
-		query = "UPDATE story SET text = %s, date = %s WHERE user_id = %d" %(storyText,curdate,userId)
-		cursor.execute(query)
+		if add == True : 
+			query = "UPDATE story SET text = '%s', date = '%s' WHERE user_id = '%d'" %(storyText,curdate,userId)
+			cursor.execute(query)
 
 	#return response
 	response['status_code'] = 200
