fac83cd68723c50058b3f479d1d9f2dd6518fb0e
web-server <links@web-server.com>
2017-05-13 11:30:19 +0300

    Tested, changed a couple of things, and now both story and delete_story are UP

--- api/newstory.py
+++ api/newstory.py
@@ -8,6 +8,7 @@ import token_encoder
 import json
 import jwt
 import time
+import base64
 
 appNewStory = Blueprint('api_newstory',__name__)
 
@@ -65,10 +66,6 @@ def newStory() :
 	#get story status
 	storyFeel = request.form.get('feel')
 
-	#get current time
-	curdate = time.strftime("%Y-%m-%d %H:%M:%S")
-	response['date'] = curdate
-
 
 	#check if user had a story posted
 	query ="SELECT user_id FROM story WHERE user_id = '%d'" % (userId)
@@ -80,19 +77,54 @@ def newStory() :
 	and (storyImage == None or storyImage == "") and (storyFeel == None or storyFeel == "") :
 		add = False
 
+	if storyImage != None and storyImage != "":
+		if "jpeg" in storyImage:
+			type = "jpeg"
+		else:
+			type = "png"
+		try:
+			storyImage = storyImage[storyImage.index(",") + 1:]
+		except ValueError:
+			response["error"] = "Bad format for image"
+			response["description"] = "Please provide the image in a base64 format"
+			response["status_code"] = 400
+			return Response(json.dumps(response, sort_keys = True), mimetype = 'application/json'),200
+		with open("/var/www/stories/story" + str(userId) + "." + type,'wb') as f:
+			f.write(base64.b64decode(storyImage))
+
+	#get current time
+	curdate = time.strftime("%Y-%m-%d %H:%M:%S")
+	response['date'] = curdate
+
+
+#	if storyText == None:
+#		storyText = "NULL"
+#	if storyFeel == None:
+#		storyFeel = "NULL"
+
 	#if no story there
 	#insert new story in db
 	if storyPresent == None :
 		if add == True :
-			query = "INSERT INTO story (user_id, text, feel, image, date) VALUES ('%d', '%s', '%s', '%s', '%s')" \
-			% (userId, storyText, storyFeel, storyImage, curdate)
+			if storyImage != None:
+				query = "INSERT INTO story (user_id, text, feel, image, date) VALUES ('%d', '%s', '%s', '%s', '%s')" \
+				% (userId, storyText, storyFeel, "story" + str(userId) + "." + type, curdate)
+			else:
+				query = "INSERT INTO story (user_id, text, feel, image, date) VALUES ('%d', '%s', '%s', '%s', '%s')" \
+				% (userId, storyText, storyFeel, storyImage, curdate)
+
 			cursor.execute(query)
 			db.commit()
 #else update old story
 	else :
 		if add == True :
-			query = "UPDATE story SET text = '%s', feel = '%s', image = '%s', date = '%s' WHERE user_id = '%d'" \
-			%(storyText, storyFeel, storyImage, curdate, userId)
+			if storyImage != None:
+				query = "UPDATE story SET text = '%s', feel = '%s', image = '%s', date = '%s' WHERE user_id = '%d'" \
+				%(storyText, storyFeel, "story" + str(userId) + "." + type, curdate, userId)
+			else:
+				query = "UPDATE story SET text = '%s', feel = '%s', image = '%s', date = '%s' WHERE user_id = '%d'" \
+				%(storyText, storyFeel, storyImage, curdate, userId)
+
 			cursor.execute(query)
 			db.commit()
 	#return response
