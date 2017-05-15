49f3f24b9ca04dab01d89fb067297d340c1440dc
Bogdanel <bogdanalexandrescu96@gmail.com>
2017-05-15 23:07:19 +0300

    no message

--- static/js/chat.js
+++ static/js/chat.js
@@ -349,182 +349,182 @@ window.onload = function() {
             // handle the invalid form...
         } else {
             event.preventDefault();
-			convertAndResizeImage("settings_photo",50,50,function(b) {
-				base64Image=b;
+            convertAndResizeImage("settings_photo", 50, 50, function(b) {
+                base64Image = b;
 
-				$.ajax({
-					method: "POST",
-					url: "http://" + ip + "/api/update",
-					headers: { Authorization: localStorage.TOKEN },
-					data: {
-						name: $("#settings_name").val(),
-						birth_day: $("#birthDay").find(":selected").text(),
-						birth_month: $("#birthMonth").find(":selected").text(),
-						birth_year: $("#birthYear").find(":selected").text(),
-						avatar: base64Image
-					},
-					dataType: "json",
-					success: function(data) {
-						$('#updateAccount').modal('hide');
-						$("#profile_name").html($("#settings_name").val());
-						if (base64Image !== undefined) {
-							$("#profile_image").attr('src', base64Image);
-						}
-						base64Image = undefined;
-					}
-				});
-			});
-		}
-	});
+                $.ajax({
+                    method: "POST",
+                    url: "http://" + ip + "/api/update",
+                    headers: { Authorization: localStorage.TOKEN },
+                    data: {
+                        name: $("#settings_name").val(),
+                        birth_day: $("#birthDay").find(":selected").text(),
+                        birth_month: $("#birthMonth").find(":selected").text(),
+                        birth_year: $("#birthYear").find(":selected").text(),
+                        avatar: base64Image
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        $('#updateAccount').modal('hide');
+                        $("#profile_name").html($("#settings_name").val());
+                        if (base64Image !== undefined) {
+                            $("#profile_image").attr('src', base64Image);
+                        }
+                        base64Image = undefined;
+                    }
+                });
+            });
+        }
+    });
 
-	
 
-	  $('#form_story').on('submit', function(event) {
+
+    $('#form_story').on('submit', function(event) {
         if (event.isDefaultPrevented()) {
             // handle the invalid form...
         } else {
             event.preventDefault();
-			convertAndResizeImage("story_photo",50,50,function(b) {
-				base64Image=b;
-				
-				$.ajax({
-					method: "POST",
-					url: "http://" + ip + "/api/new_story",
-					headers: { Authorization: localStorage.TOKEN },
-					data: {
-						text: $("#story_status").val(),
-						birth_day: $("#story_feel").find(":selected").text(),
-						image: base64Image
-					},
-					dataType: "json",
-					success: function(data) {
-						$("#story_status").val("");
-						$("#story_feel").val([]);
-						base64Image = undefined;
+            convertAndResizeImage("story_photo", 50, 50, function(b) {
+                base64Image = b;
 
-						showStory(null,$('#form_story'));
-						var deleteButton = $('<div class="wrapper"><button type="button" class="btn btn-danger btn-lg" class="form-group">Delete story</button></div>');
+                $.ajax({
+                    method: "POST",
+                    url: "http://" + ip + "/api/new_story",
+                    headers: { Authorization: localStorage.TOKEN },
+                    data: {
+                        text: $("#story_status").val(),
+                        birth_day: $("#story_feel").find(":selected").text(),
+                        image: base64Image
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        $("#story_status").val("");
+                        $("#story_feel").val([]);
+                        base64Image = undefined;
 
-						 setTimeout(function(){
-							 
-							$('#form_story').append(deleteButton).css("width:auto");
-						 },500);
-						
-						deleteButton.on('click', '', function() {
+                        showStory(null, $('#form_story'));
+                        var deleteButton = $('<div class="wrapper"><button type="button" class="btn btn-danger btn-lg" class="form-group">Delete story</button></div>');
 
+                        setTimeout(function() {
 
-								 $.ajax({
-            					method: "POST",
-           						 url: "http://" + ip + "/api/delete_story",
-           						 headers: { Authorization: localStorage.TOKEN },
-           						 dataType: "json",
-            					success: function(data) {
+                            $('#form_story').append(deleteButton).css("width:auto");
+                        }, 500);
 
-									$('#editStory').modal('toggle');
-       							    document.getElementById("form_story").removeChild(document.getElementById("form_story").lastElementChild);
-									document.getElementById("form_story").removeChild(document.getElementById("form_story").lastElementChild);
-    							}
-								
-	
-							});
-
-						});
-
-					}
-				});
-
-				$('#editStory').modal('toggle');
+                        deleteButton.on('click', '', function() {
 
 
-			});
-		}
-	});
-	
-	 $.ajax({
+                            $.ajax({
+                                method: "POST",
+                                url: "http://" + ip + "/api/delete_story",
+                                headers: { Authorization: localStorage.TOKEN },
+                                dataType: "json",
+                                success: function(data) {
+
+                                    $('#editStory').modal('toggle');
+                                    document.getElementById("form_story").removeChild(document.getElementById("form_story").lastElementChild);
+                                    document.getElementById("form_story").removeChild(document.getElementById("form_story").lastElementChild);
+                                }
+
+
+                            });
+
+                        });
+
+                    }
+                });
+
+                $('#editStory').modal('toggle');
+
+
+            });
+        }
+    });
+
+    $.ajax({
         method: "GET",
         url: "http://" + ip + "/api/story",
         headers: { Authorization: localStorage.TOKEN },
         dataType: "json",
         success: function(data) {
 
-				if(data.text!=null || data.image!=null || data.feel!=null)
-					{
-
-						
-						showStory(null,$('#form_story'));
-
-						var deleteButton = $('<div class="wrapper"><button type="button" class="btn btn-danger btn-lg" class="form-group">Delete story</button></div>');
-
-						 setTimeout(function(){
-							 
-							$('#form_story').append(deleteButton).css("width:auto");
-						 },100);
-						
-						deleteButton.on('click', '', function() {
+            if (data.text != null || data.image != null || data.feel != null) {
 
 
-								 $.ajax({
-            					method: "POST",
-           						 url: "http://" + ip + "/api/delete_story",
-           						 headers: { Authorization: localStorage.TOKEN },
-           						 dataType: "json",
-            					success: function(data) {
+                showStory(null, $('#form_story'));
 
-									
-									$('#editStory').modal('toggle');
+                var deleteButton = $('<div class="wrapper"><button type="button" class="btn btn-danger btn-lg" class="form-group">Delete story</button></div>');
 
-									document.getElementById("form_story").removeChild(document.getElementById("form_story").lastElementChild);
-									document.getElementById("form_story").removeChild(document.getElementById("form_story").lastElementChild);
-									}
-							});
+                setTimeout(function() {
 
-						});
-					}
+                    $('#form_story').append(deleteButton).css("width:auto");
+                }, 100);
 
-		}});
+                deleteButton.on('click', '', function() {
 
-	$('#form_password').validator().on('submit', function(event) {
-		if (event.isDefaultPrevented()) {
-			// handle the invalid form...
-			} else {
-			event.preventDefault();
-			$.ajax({
-				method: "POST",
-				url: "http://" + ip + "/api/update",
-				headers: { Authorization: localStorage.TOKEN },
-				data: { password: $("#settings_password").val() },
-				dataType: "json",
-				success: function(data) {
-					$("#settings_password").val("");
-					$("#settings_password2").val("");
-					$('#changePass').modal('hide');
-				}
-			});
-		}
-	});
-	
-	$('#form_deleteAccount').validator().on('submit', function(event) {
-		if (event.isDefaultPrevented()) {
-			// handle the invalid form...
-			} else {
-			event.preventDefault();
-			$.ajax({
-				method: "POST",
-				url: "http://" + ip + "/api/delete_account",
-				headers: { Authorization: localStorage.TOKEN },
-				data: { email: localStorage.EMAIL },
-				dataType: "json",
-				success: function(data) {
-					var alert = document.createElement("div");
-					alert.innerHTML='<div class="alert alert-warning" role="alert"><strong>Warning!</strong> This account will be deleted. Please check your e-mail.</div>';
-					document.getElementsByTagName('body')[0].append(alert);
-					$('#deleteAccount').modal('hide');
-				}
-			});
-		}
-	});
-	
-	
+
+                    $.ajax({
+                        method: "POST",
+                        url: "http://" + ip + "/api/delete_story",
+                        headers: { Authorization: localStorage.TOKEN },
+                        dataType: "json",
+                        success: function(data) {
+
+
+                            $('#editStory').modal('toggle');
+
+                            document.getElementById("form_story").removeChild(document.getElementById("form_story").lastElementChild);
+                            document.getElementById("form_story").removeChild(document.getElementById("form_story").lastElementChild);
+                        }
+                    });
+
+                });
+            }
+
+        }
+    });
+
+    $('#form_password').validator().on('submit', function(event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+        } else {
+            event.preventDefault();
+            $.ajax({
+                method: "POST",
+                url: "http://" + ip + "/api/update",
+                headers: { Authorization: localStorage.TOKEN },
+                data: { password: $("#settings_password").val() },
+                dataType: "json",
+                success: function(data) {
+                    $("#settings_password").val("");
+                    $("#settings_password2").val("");
+                    $('#changePass').modal('hide');
+                }
+            });
+        }
+    });
+
+    $('#form_deleteAccount').validator().on('submit', function(event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+        } else {
+            event.preventDefault();
+            $.ajax({
+                method: "POST",
+                url: "http://" + ip + "/api/delete_account",
+                headers: { Authorization: localStorage.TOKEN },
+                data: { email: localStorage.EMAIL },
+                dataType: "json",
+                success: function(data) {
+                    var alert = document.createElement("div");
+                    alert.innerHTML = '<div class="alert alert-warning" role="alert"><strong>Warning!</strong> This account will be deleted. Please check your e-mail.</div>';
+                    document.getElementsByTagName('body')[0].append(alert);
+                    $('#deleteAccount').modal('hide');
+                }
+            });
+        }
+    });
+
+
 }
 
 function refreshTokenRequest() {
